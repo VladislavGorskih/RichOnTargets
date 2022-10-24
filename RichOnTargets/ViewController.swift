a8684bae9e0392486fa4b8d2dfe8aeedf74caa32
Gorskih <100698066+VladislavGorskih@users.noreply.github.com>
2022-10-24 20:01:52 +0300

    1

--- RichOnTargets/ViewController.swift
+++ RichOnTargets/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    
+    var game: Game!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
-
-
+    
+    @IBAction func checkNumber() {
+        game.calculateScore(with: Int(slider.value))
+        if game.isGameEnded {
+            showAlertWith(score: game.score)
+            game.restartGame()
+        } else {
+            game.startNewRound()
+        }
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
+    }
+    
+    private func updateLabelWithSecretNumber(newText: String) {
+        label.text = newText
+    }
+    
+    private func showAlertWith(score: Int) {
+        let alert = UIAlertController(title: "Игра окончена",
+                                      message: "Вы заработали \(score) очков",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
